cda71e0469b601db17c6273f7bb1526ed0d1a279
UmikoCodes <111806862+MiniCodes@users.noreply.github.com>
2023-05-05 14:57:06 -0600

    Upgraded Button Calculator
    ~ Added more functions to the button Calculator
    ~ Updated Calculator JavaScript to support the new functions
    ~ Updated CSS & button calculator styling

--- Math/Math Calculator/mathcal.js
+++ Math/Math Calculator/mathcal.js
@@ -88,12 +88,105 @@ function CalDivide() {
 
 
 
-// function to display value of calculator buttons
-function display(val) {
+
+/* function to display value of Scientific calculator
+buttons */
+function sciDisplay(val) {
     document.getElementById("calResults").value += val
 }
 
-// function to solve equation in button calculator
-function solve() {
+// function to solve equation in Scientific calculator
+function sciSolve() {
     calResults.value = eval(calResults.value)
+}
+
+/* function to delete 1 character from the Science 
+Calculator; a backspace button */
+function backspace() {
+    calResults.value = calResults.value.substr(0, calResults.value.length-1);
+}
+
+
+// adds the Trig. sin function to science calculator
+function sin() {
+    calResults.value = Math.sin(calResults.value);
+}
+
+// adds the Trig. cos function to science calculator
+function cos() {
+    calResults.value = Math.cos(calResults.value);
+}
+
+// adds the Trig. tan function to science calculator
+function tan() {
+    calResults.value = Math.tan(calResults.value);
+}
+
+
+// adds the PI function to science calculator
+function pi() {
+    calResults.value += Math.PI;
+}
+
+
+/* function that square roots the current numbers in science 
+calculator */
+function sqrt() {
+    calResults.value = Math.sqrt(calResults.value, 2);
+}
+
+// squares the current numbers in science calculator by 2
+function squared() {
+    calResults.value = Math.pow(calResults.value, 2);
+}
+
+/* function finds the factorial of a positive integer; finds 
+the factorial of the numbered entered in science calculator */
+function fact() {
+    var i, num, f;
+
+    f = 1;
+    num = calResults.value;
+
+    for(i=1; i<=num; i++) {
+        f = f*i;
+    }
+
+    i = i-1;
+
+    calResults.value = f;
+}
+
+
+// adds the log function to science calculator
+function log() {
+    calResults.value = Math.log(calResults.value);
+}
+
+// Adds the log base 2 function to science calculator 
+function logBase2() {
+    calResults.value = Math.log2(calResults.value);
+}
+
+// Adds the log base 10 function to science calculator 
+function logBase10() {
+    calResults.value = Math.log10(calResults.value);
+}
+
+
+/* Function takes 10 to the power of what the user enters into
+the science calcualtor */
+function tenPowers() {
+    calResults.value = Math.pow(10, calResults.value);
+}
+
+/* function returns the exponent of what's entered into the
+science calculator */
+function exponent() {
+    calResults.value = Math.exp(calResults.value);
+}
+
+// function finds the absolute of the entered number
+function absolute() {
+    calResults.value = Math.abs(calResults.value);
 }
