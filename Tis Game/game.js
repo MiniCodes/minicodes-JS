c08c898556584e89326c039268ec068ce8a11b34
UmikoCodes <111806862+MiniCodes@users.noreply.github.com>
2023-05-09 23:51:56 -0600

    Updated Tis Game
    ~ The game now counts the user's amount of guesses
    ~ Added JavaScript for guess counting
    ~ Updated Tis Game html

--- Tis Game/game.js
+++ Tis Game/game.js
@@ -1,6 +1,9 @@
 // sets random number to 0
 var randomNum = 0
 
+// sets the user's number of guesses to 0
+var numofGuess = 0
+
 /* function to choose & store a random number */
 function chooseRandoNum(min, max) {
     // sets random number to chosen random number
@@ -9,6 +12,18 @@ function chooseRandoNum(min, max) {
 
     // converts random number to an integer
     randomNum = parseInt(randomNum)
+
+
+    /* resets the user's number of guesses to zero */
+    numofGuess = 0
+
+
+    // clears previous game's messages
+    document.getElementById('checkResults').innerHTML = '';
+
+    /* clears previous game's number of guesses 
+    message */
+    document.getElementById('guessResults').innerHTML = '';
 
     // Used to see Random Number for testing
     /* document.getElementById("randoResults").innerHTML = 
@@ -25,11 +40,19 @@ function checkRandoNum() {
     guessNum = parseInt(guessNum)
 
     if (guessNum < randomNum) {
+        /* adds 1 to the number of guesses made by
+        the user */
+        numofGuess = numofGuess + 1
+
         // message for when guess is too low
         document.getElementById("checkResults").innerHTML = 
         "Sorry, this isn't the number." + "<br>" +
         "Try doing a higher number :3";
     } if (guessNum > randomNum) {
+        /* adds 1 to the number of guesses made by 
+        the user */
+        numofGuess = numofGuess + 1
+
         // message for when guess is too high
         document.getElementById("checkResults").innerHTML = 
         "Sadly this isn't the number." + "<br>" +
@@ -39,5 +62,10 @@ function checkRandoNum() {
         document.getElementById("checkResults").innerHTML = 
         "You did it! You guessed the correct number!" + 
         "<br>" + "Choose another random number to play again";
+
+        /* message for how many guesses a user made to 
+        guess the correct number */
+        document.getElementById('guessResults').innerHTML = 
+        "It took you " + numofGuess + " guesses :3";
     }
 }
