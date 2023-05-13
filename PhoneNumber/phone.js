8bac240325f65ab68cdebab3fba4976fb29b7a39
UmikoCodes <111806862+MiniCodes@users.noreply.github.com>
2023-05-13 16:45:03 -0600

    Added Content for Telephone Project
    ~ Started html for Telephone project
    ~ Started JS for Telephone project

--- PhoneNumber/phone.js
+++ PhoneNumber/phone.js
@@ -1,3 +1,23 @@
-function checkContactInfo() {
+var person = {firstName:'', lastName:'', address:'', email:'', phoneNum:''};
+
+// Creates new user account
+function createNewAcc() {
+    person.firstName = document.getElementsByName('userFName');
+
+    person.lastName = document.getElementsByName('userLName');
+
+    person.address = document.getElementsByName('userHomeAddress');
+}
+
+// prints contact information
+function printContactInfo() {
+
+    createNewAcc()
     
+
+    document.getElementById('contactPrint').innerHTML = 
+    "Hello " + person.firstName + "" + person.lastName + 
+    ", thank you for making a new account with us!" + "<br>" +
+    "We will be sending a welcome package to this address: " + 
+    person.address + "!"
 }
